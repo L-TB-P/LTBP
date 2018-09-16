89a5eb69451f0fa8400e7cfde947d4f4a65c3094
OedorRats <kaorunba_14@me.com>
2018-09-16 11:11:46 +0900

    hggghh

--- Lega/Lega_xPhone/config.hpp
+++ Lega/Lega_xPhone/config.hpp
@@ -2,7 +2,7 @@ class xPhoneMain {
   phoneNumberPrefix = "+09";//the prefix for you phone numbers.
   phoneNumberLength = 8; // the amount of numbers after the prefix.
   phoneNumberStart = 0;// the first number after the prefix // -1 if it doesn't matter.
-  ServerTag = "ptbl";//PLX make this uniqeee
+  ServerTag = "ltbp";//PLX make this uniqeee
   olduseItem = false;// set this to tru if you want to use the old useItem Function
   //Please Don't change this if you don't know what you are doing, this is to intergrate control groups into the animations
   allMenus[] = {
